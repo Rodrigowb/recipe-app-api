124d445ad4cc840677ffde43cd7ada3d47f804a1
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2022-05-30 14:38:38 -0300

    installing psycopg2 and rebuilding the docker container

--- app/app/test.py
+++ app/app/test.py
@@ -1,15 +1,3 @@
-from django.test import SimpleTestCase
 
-from app import calc
-
-class CalcTests(SimpleTestCase):
-
-  def test_add_numbers(self):
-    res = calc.add(5, 6)
-    self.assertEqual(res, 11)
-  
-  def test_subtract_numbers(self):
-    res = calc.sub(5, 3)
-    self.assertEqual(res, 2)
 
     
