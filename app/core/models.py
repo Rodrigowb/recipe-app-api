c2cd450e0d422c564b54c4e8b18c10747b3fd01b
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2022-06-01 19:20:06 -0300

    adding the django admin to the project

--- app/core/models.py
+++ app/core/models.py
@@ -13,14 +13,26 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user"""
+        # Make all users has email
+        if not email:
+            raise ValueError('Users must have an email address.')
         # Define a new user
-        user = self.model(email=email, **extra_fields)
+        # Including email normalized method (provided by the base user manager)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         # Support multiple dbs to the same project (edge case...)
         user.save(using=self._db)
 
         return user
 
+    def create_superuser(self, email, password):
+        """Create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
 
 # Creating user model
 # AbstractBaseUser contains the functionalities for authentication
