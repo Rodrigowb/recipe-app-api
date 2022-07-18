6f28e3770c2a2673b440019aa341949de97d452d
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2022-07-18 11:48:23 -0300

    implementing the authentication API and tests for manage user API

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -13,7 +13,7 @@ class AdminSiteTests(TestCase):
 
   # Write the setup to the tests, creating a user and a superuser, authenticated
     def setUp(self):
-        """Create uer and client"""
+        """Create user and client"""
         self.client = Client()
         # Create superuser
         self.admin_user = get_user_model().objects.create_superuser(
